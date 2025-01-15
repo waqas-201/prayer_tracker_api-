6670587765a58d339c57efd7ba1bcd35e0f92ae1
511991waqas <waqasvu892@gmail.com>
2025-01-15 19:15:56 +0500

    create prayer feature + some refector

--- src/prayers/dto/create-prayer.dto.ts
+++ src/prayers/dto/create-prayer.dto.ts
@@ -1,9 +1,13 @@
-import { PrayerName } from '@prisma/client';
+import { MenPrayerStatus, PrayerName, WomenPrayerStatus } from '@prisma/client';
 import { createZodDto } from 'nestjs-zod';
 import { z } from 'zod';
 
 const createPrayerSchema = z.object({
   prayerName: z.nativeEnum(PrayerName),
   profileId: z.string(),
+  prayerStatus: z.union([
+    z.nativeEnum(MenPrayerStatus),
+    z.nativeEnum(WomenPrayerStatus),
+  ]),
 });
 export class CreatePrayerDto extends createZodDto(createPrayerSchema) {}
--- src/prayers/providers/createPrayer.provider.ts
+++ src/prayers/providers/createPrayer.provider.ts
@@ -5,7 +5,15 @@ import {
 } from '@nestjs/common';
 import { CreatePrayerDto } from '../dto/create-prayer.dto';
 import { PrismaService } from 'src/prismaModule/prisma.service';
-import { format, isAfter, isBefore, isEqual } from 'date-fns';
+import {
+  endOfDay,
+  format,
+  isAfter,
+  isBefore,
+  isEqual,
+  startOfDay,
+} from 'date-fns';
+import { Gender, MenPrayerStatus, WomenPrayerStatus } from '@prisma/client';
 
 //get user id from request
 // from this user id get profile id
@@ -22,6 +30,8 @@ import { format, isAfter, isBefore, isEqual } from 'date-fns';
 // get prayer status user is trying to mark
 // check user gender has this gender has valid to create  this prayer status
 // calculte time  is this time is valid for this status
+
+//----------------------this feature will be implemented later---------------------------
 //if gender is men
 // 30 minuts ---->  from start prayer time is MEN Prayerstatus = TAKBEERE_E_ULA
 // 45 minuts ---->  from start prayer time is MEN Prayerstatus = SALATAL_JAMAAH
@@ -32,95 +42,169 @@ import { format, isAfter, isBefore, isEqual } from 'date-fns';
 // 45 minuts ---->  from start prayer time is WOMEN Prayerstatus = PRAYED_LATE
 // 45+  minuts ---->  from start prayer time is WOMEN Prayerstatus = PRAYED_TOO_LATE
 // end time of prayer ----> means user missed the prayer timings to he only can mark status for MISSED_PRAYER
+//----------------------this feature will be implemented later---------------------------
+
 // if all conditions are valid then create prayer status
 
 @Injectable()
 export class CreatePrayerProvider {
   constructor(private readonly prismaService: PrismaService) {}
 
-  async createPrayer(createPrayerDto: CreatePrayerDto, user: any) {
-    //get prfileId from request
-    //from logged in user  userId get profileId
-    // it means one profileId user provided me and one i quered by logged in user userId
-    const profile = await this.prismaService.profile.findFirst({
-      where: { userId: user.userId },
-    });
-
-    if (!profile) {
-      throw new BadRequestException('Profile not found');
-    }
-
+  private checkProfileIds(
+    profileIdFromDb: string,
+    userProviderProfileId: string,
+  ) {
     // check user providing profile id is same as we get profile id by logged in user userid
     const isUserProvidingProfileIdSameAsLoggedInUser =
-      profile.id === createPrayerDto.profileId;
+      profileIdFromDb === userProviderProfileId;
     // if not then throw error
     if (!isUserProvidingProfileIdSameAsLoggedInUser) {
       throw new BadRequestException(
         'Provided profile id is not same as logged in user profile id',
       );
     }
+  }
 
-    // get user gender from profile
-    const gender = profile.gender;
-    // now get for which prayer user is trying to mark status
-    //  for example if user is trying to mark status for fajr prayer
-    // check is this time is valid for fajr prayer
-    // get all prayers by user
-    const loggedInUserPrayerTimings =
-      await this.prismaService.prayerTimings.findFirst({
-        where: {
-          profileId: createPrayerDto.profileId,
-        },
+  private checkIsUserPrayerStatusIsValid(
+    gender: Gender,
+    statusValue: MenPrayerStatus | WomenPrayerStatus,
+  ) {
+    if (gender === 'MEN') {
+      return Boolean(MenPrayerStatus[statusValue]);
+    }
+    if (gender === 'WOMEN' || gender === 'OTHER') {
+      return Boolean(MenPrayerStatus[statusValue]);
+    }
+  }
+
+  private async getLoggedInUserTimings(profileId: string) {
+    try {
+      const loggedInUserPrayerTimings =
+        await this.prismaService.prayerTimings.findFirst({
+          where: {
+            profileId,
+          },
+        });
+
+      if (!loggedInUserPrayerTimings) {
+        // get user location from user profile and fetch prayers and add them into db
+        // for now thrwo an error
+        throw new InternalServerErrorException('filed to find prayer timings');
+      }
+
+      return loggedInUserPrayerTimings;
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  private async getUserProfile(userId: string) {
+    try {
+      const profile = await this.prismaService.profile.findFirst({
+        where: { userId: userId },
       });
 
-    if (!loggedInUserPrayerTimings) {
-      // get user location from user profile and fetch prayers and add them into db
-      // for now thrwo an error
-      throw new InternalServerErrorException('filed to find prayer timings');
+      if (!profile) {
+        throw new BadRequestException('Profile not found');
+      }
+      return profile;
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  //checks in current date user marked prayer if marked  throw error so user can not mark one prayer two times
+  private async checkIfPrayerAlreadyMarked(createPrayerDto: CreatePrayerDto) {
+    const currentDateAndTime = new Date();
+    try {
+      const DoseAlreadyMarkedSamePrayer =
+        await this.prismaService.prayer.findFirst({
+          where: {
+            profileId: createPrayerDto.profileId,
+            prayerName: createPrayerDto.prayerName,
+            createdAt: {
+              gte: startOfDay(currentDateAndTime),
+              lte: endOfDay(currentDateAndTime),
+            },
+          },
+        });
+      // if yes then throw error
+      if (DoseAlreadyMarkedSamePrayer) {
+        throw new BadRequestException('you already have marked your prayer');
+      }
+
+      return DoseAlreadyMarkedSamePrayer;
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  async createPrayer(createPrayerDto: CreatePrayerDto, user: any) {
+    //get prfileId from request
+    // from logged in user  userId get profileId
+    // it means one profileId user provided to me and one i got  from logged in user
+    const profile = await this.getUserProfile(user.userId);
+    // check user providing profile id is same as we get profile id by logged in user userid
+    this.checkProfileIds(profile.id, createPrayerDto.profileId);
+
+    const isUserPrayerStatusIsValid = this.checkIsUserPrayerStatusIsValid(
+      profile.gender,
+      createPrayerDto.prayerStatus,
+    );
+    if (!isUserPrayerStatusIsValid) {
+      throw new BadRequestException('invalid prayer status ');
     }
 
-    // first get user is trying to mark status for which prayer
-    // Check if the prayer start time has not arrived yet or if the prayer time has ended
+    // get all prayers by user
+    const loggedInUserPrayerTimings = await this.getLoggedInUserTimings(
+      profile.id,
+    );
 
+    //  now get for which prayer user is trying to mark status
+    //  for example if user is trying to mark status for fajr prayer
+    // check is this time is valid for fajr prayer
+    // Check if the prayer start time has not arrived yet or if the prayer time has ended
     // then get start and end time for this prayer from db
     const startTime =
       loggedInUserPrayerTimings[
-        createPrayerDto.prayerName.toLocaleLowerCase().concat('Start')
+        createPrayerDto?.prayerName?.toLocaleLowerCase().concat('Start')
       ];
     const endTime =
       loggedInUserPrayerTimings[
-        createPrayerDto.prayerName.toLocaleLowerCase().concat('End')
+        createPrayerDto?.prayerName?.toLocaleLowerCase().concat('End')
       ];
 
-    const currentTime = new Date();
-    const prayerTimeIsNotStartedYet = isBefore(currentTime, startTime);
-    const prayerTimeHasEnded = isAfter(currentTime, endTime);
-    const prayerTimeIsStartingNow = isEqual(currentTime, startTime);
+    const currentDateAndTime = new Date();
+    //checks in current date user marked prayer if marked  throw error so user can not mark one prayer two times
+    await this.checkIfPrayerAlreadyMarked(createPrayerDto);
 
-    if (prayerTimeHasEnded) {
-      throw new BadRequestException('you missed the prayer');
-    } else if (prayerTimeIsNotStartedYet) {
+    const prayerTimeIsNotStartedYet = isBefore(currentDateAndTime, startTime);
+    const prayerTimeHasEnded = isAfter(currentDateAndTime, endTime);
+    if (prayerTimeIsNotStartedYet) {
       throw new BadRequestException('prayer time is not started yet ');
+    } else if (prayerTimeHasEnded) {
+      throw new BadRequestException('you missed the prayer');
     }
 
-    // now check is this user already marked status for this prayer
-    // if yes then throw error
     // if time is valid and status is not marked then
-    // get prayer status user is trying to mark
-    // check user gender has this gender has valid to create  this prayer status
-    // calculte time  is this time is valid for this status
-    //if gender is men
-    //  is MEN Prayerstatus = TAKBEERE_E_ULA
-    //  is MEN Prayerstatus = SALATAL_JAMAAH
-    //  is MEN Prayerstatus = INDIVIDUAL_PRAYER
-    //  is MEN Prayerstatus = MISSED
-    //  is MEN Prayerstatus = QAZA
-    //if gender is women or other
-    //  is WOMEN Prayerstatus = PRAYED_AT_TIME
-    //  is WOMEN Prayerstatus = PRAYED_LATE
-    //  is WOMEN Prayerstatus = PRAYED_TOO_LATE
-    //  is WOMEN Prayerstatus = MISSED
-    //  is WOMEN Prayerstatus = QAZA
     // if all conditions are valid then create prayer status
+
+    const isMen = Boolean(profile.gender === 'MEN');
+
+    const markPrayerStatus = await this.prismaService.prayer.create({
+      data: {
+        performedAt: currentDateAndTime,
+        prayerName: createPrayerDto.prayerName,
+        profileId: createPrayerDto.profileId,
+        ...(isMen
+          ? {
+              menStatus: createPrayerDto.prayerStatus as MenPrayerStatus,
+            }
+          : { womenStatus: createPrayerDto.prayerStatus as WomenPrayerStatus }),
+      },
+    });
+
+    return markPrayerStatus;
   }
 }
+
